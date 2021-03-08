14301dcc6e74e142899f4a4672fc200f54cbdf09
JesseNReynolds <jessenreynolds@gmail.com>
2021-03-08 15:03:00 -0700

    Won't be using cookies, changed gid to fbid

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_210533) do
+ActiveRecord::Schema.define(version: 2021_03_08_215854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2021_02_23_210533) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "gid"
+    t.string "fb_id"
     t.string "settings_obj"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
