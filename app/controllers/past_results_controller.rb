b1c895d7038dd738651e8be44e63c31c84bb860e
JesseNReynolds <jessenreynolds@gmail.com>
2021-03-09 12:49:00 -0700

    gut routes and controllers from controller, keep/modify remaining methods

--- app/controllers/past_results_controller.rb
+++ app/controllers/past_results_controller.rb
@@ -1,5 +1,4 @@
 class PastResultsController < ApplicationController
-  before_action :set_past_result, only: [:show, :update, :destroy]
 
   # GET /past_results
   def index
@@ -8,15 +7,13 @@ class PastResultsController < ApplicationController
     render json: @past_results
   end
 
-  # GET /past_results/1
-  def show
-    render json: @past_result
-  end
 
   # POST /past_results
   def create
     @past_result = PastResult.new(past_result_params)
 
+    @past_result[:user_id] = user_id_from_fb_id(@past_result[:user_id])
+    
     if @past_result.save
       render json: @past_result, status: :created, location: @past_result
     else
@@ -24,26 +21,7 @@ class PastResultsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /past_results/1
-  def update
-    if @past_result.update(past_result_params)
-      render json: @past_result
-    else
-      render json: @past_result.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /past_results/1
-  def destroy
-    @past_result.destroy
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_past_result
-      @past_result = PastResult.find(params[:id])
-    end
-
     # Only allow a list of trusted parameters through.
     def past_result_params
       params.require(:past_result).permit(:user_id, :yelp_id, :rating, :restaurant_name)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :past_results
-  resources :users
+  resources :past_results, only: [:create]
+  
+  resources :users, only: [:create] do
+    resources :past_results, only [:index]
+  end
+
   post '/query', to: 'yelp_query#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
